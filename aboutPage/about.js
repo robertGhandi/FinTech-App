c3891cfc3fa345d79d5ac8c69a9e2d997dbeb638
robertGhandi <robertzerah@gmail.com>
2024-11-12 04:39:17 +0100

    updated files

---
+++ aboutPage/about.js
@@ -0,0 +1,11 @@
+function back() {
+    window.location.href = "../homePage/homePage.html"
+}
+
+function reward() {
+    window.location.href = "../Rewards/rewards.html"
+}
+
+function home() {
+    window.location.href = "../homePage/homePage.html"
+}
